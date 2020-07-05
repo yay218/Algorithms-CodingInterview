7e89cbc6b67e2c4e220a21dcefcf06f7a68a87e7
Yang Yi <yiyang1996@outlook.com>
2020-07-04 17:29:15 -0700

    bfs1

--- 1 Breath First Search/1_BinaryTreeLevelOrderTraversal.java
+++ 1 Breath First Search/1_BinaryTreeLevelOrderTraversal.java
@@ -1,1 +1,65 @@
-// Binary Tree Level Order Traversal
+/*
+Binary Tree Level Order Traversal
+Leetcode #: 102
+Lintcode #: 69
+
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+*/
+
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Level order a list of lists of integer
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // write your code here
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            List<Integer> currLevel = new ArrayList<>();
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currNode = queue.poll();
+                currLevel.add(currNode.val);
+                if (currNode.left != null) {
+                    queue.offer(currNode.left);
+                }
+                if (currNode.right != null) {
+                    queue.offer(currNode.right);
+                }
+            }
+            result.add(currLevel);
+        }
+        return result;
+    }
+}
+
+
